ddab75fcc1ace9733d5be8811f2ce1cdf714121b
Kevin Shao <shaoqihen@gmail.com>
2024-02-19 15:02:25 +0000

    First

---
+++ firstprogram.cpp
@@ -0,0 +1,13 @@
+/*This is a comment
+Following C++ a Beginner's Guide by Hebert Schildt
+*/
+#include <iostream>
+//namespace is declarative region where elemenets can be placed
+using namespace std;
+// A C++ program begins at main().
+int main()
+{
+    cout << "C++ is power programming";
+
+    return 0;
+}
